d0c2b393a359871a294cb9892cacafe8e22a1325
JosiasPires <supergc59@gmail.com>
2023-02-13 19:32:52 -0300

    Make grid cells automatically change size

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const grid = document.querySelector('.grid');
-for (let i = 0; i < 25; i++) {
+for (let i = 0; i < 64**2; i++) {
     const createCell = document.createElement('div');
     createCell.setAttribute('class', 'gridCell');
     grid.appendChild(createCell);
 }
-
-const cells = Array.from(document.querySelectorAll('.gridCell'));
