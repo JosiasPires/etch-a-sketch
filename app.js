e167a342b204e4805c2504efe82087d50bca190e
JosiasPires <supergc59@gmail.com>
2023-02-13 20:00:41 -0300

    create a createGrid function

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const grid = document.querySelector('.grid');
-for (let i = 0; i < 64**2; i++) {
-    const createCell = document.createElement('div');
-    createCell.setAttribute('class', 'gridCell');
-    grid.appendChild(createCell);
+
+function createGrid(size) {
+    while (grid.firstChild) {
+        grid.firstChild.remove()
+    }
+    for (let i = 0; i < size**2; i++) {
+        const createCell = document.createElement('div');
+        createCell.setAttribute('class', 'gridCell');
+        grid.appendChild(createCell);
+    }
+    const cells = Array.from(document.querySelectorAll('.gridCell'));
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        })
+    })
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 }
 
-const cells = Array.from(document.querySelectorAll('.gridCell'));
+createGrid(16)
 
-cells.forEach(cell => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'black';
-    })
+const sizeButton = document.querySelector('#sizeButton');
+
+sizeButton.addEventListener('click', () => {
+    let gridSize = prompt('Change grid size');
+    createGrid(gridSize);
 })
