99c7896c114b6f9353855b007caf9d16055e868f
JosiasPires <supergc59@gmail.com>
2023-02-13 19:33:34 -0300

    Add hover event listener to change bgColor

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ for (let i = 0; i < 64**2; i++) {
     createCell.setAttribute('class', 'gridCell');
     grid.appendChild(createCell);
 }
+
+const cells = Array.from(document.querySelectorAll('.gridCell'));
+
+cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'black';
+    })
+})
