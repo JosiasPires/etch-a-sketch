8fa1f3fabe402ec4a4838d79a6eb0809a33607df
JosiasPires <supergc59@gmail.com>
2023-02-13 20:16:57 -0300

    Add limitations to sizeButton

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const grid = document.querySelector('.grid');
 
 function createGrid(size) {
     while (grid.firstChild) {
-        grid.firstChild.remove()
+        grid.firstChild.remove();
     }
     for (let i = 0; i < size**2; i++) {
         const createCell = document.createElement('div');
@@ -15,8 +15,8 @@ function createGrid(size) {
             cell.style.backgroundColor = 'black';
         })
     })
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
 
 createGrid(16)
@@ -25,5 +25,17 @@ const sizeButton = document.querySelector('#sizeButton');
 
 sizeButton.addEventListener('click', () => {
     let gridSize = prompt('Change grid size');
+    while (gridSize > 100 || isNaN(gridSize)) {
+        if (gridSize > 100) {
+            alert('Maximum grid size: 100');
+        }
+        if (typeof gridSize !== 'number') {
+            alert('Digit a number')
+        }
+        gridSize = prompt('Change grid size');
+    }
+    if (!gridSize) {
+        gridSize = 16
+    }
     createGrid(gridSize);
 })
